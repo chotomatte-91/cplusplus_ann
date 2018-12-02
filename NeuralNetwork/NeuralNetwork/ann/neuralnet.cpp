e61d0ae7a3ea42d600c11eae48d96882f19ec699
yuhong270991 <yuhong270991@gmail.com>
2018-12-02 18:44:45 +0800

    Updated with backpropagation

--- NeuralNetwork/NeuralNetwork/ann/neuralnet.cpp
+++ NeuralNetwork/NeuralNetwork/ann/neuralnet.cpp
@@ -14,6 +14,7 @@ Neuron::Neuron(unsigned numOutputs, unsigned index) :
   {
     m_edges.push_back(Synapse());
     m_edges.back().weight = rng.random_prob();
+    m_edges.back().deltaWeight = 0.f;
   }
 }
 
@@ -33,7 +34,7 @@ float Neuron::getOutput() const
   return m_output;
 }
 
-void Neuron::updateWeights(const Layer& previousLayer)
+void Neuron::calculateOutput(const Layer& previousLayer)
 {
   float innerproduct = 0.f;
 
@@ -44,6 +45,39 @@ void Neuron::updateWeights(const Layer& previousLayer)
   //y = f(x), where x is the innerproduct calculated previously
   assert(m_func != nullptr);
   m_output = m_func(innerproduct);
+}
+
+void Neuron::computeOutputGradients(float expectedValue)
+{
+  float delta = expectedValue - m_output;
+  m_gradient = delta * m_derivative(m_output);
+}
+
+
+void Neuron::computeHiddenGradients(const Layer& nextLayer)
+{
+  float dW = 0.f;
+
+  //sum contribution of errors at nodes of next layer
+  for (size_t i = 0; i < nextLayer.size(); ++i)
+    dW += m_edges[i].weight * nextLayer[i].m_gradient;
+
+  m_gradient = dW * m_derivative(m_output);
+}
+
+void Neuron::updateWeights(Layer& prevLayer)
+{
+  float alpha = 0.f;
+
+  //update the weights in the previous layer
+  for (size_t i = 0; i < prevLayer.size(); ++i)
+  {
+    Neuron& prevNeuron = prevLayer[i];
+    float new_deltaWeight = alpha * m_gradient * prevNeuron.getOutput();
+
+    prevNeuron.m_edges[m_index].deltaWeight = new_deltaWeight;
+    prevNeuron.m_edges[m_index].weight += new_deltaWeight;
+  }
 }
 
 Neuron& NeuralNet::getNeuron(unsigned layerNum, unsigned index)
@@ -65,9 +99,10 @@ NeuralNet::NeuralNet(const std::vector<unsigned>& config)
 
     //extra layer for bias neuron
     for (size_t n = 0; n <= config[l]; ++n)
-    {
       m_layers.back().emplace_back(numOuts, n);
-    }
+
+    //set output of bias neuron to 1.0, nothing will change its output value
+    m_layers.back().back().setOutput(1.f);
   }
 }
 
@@ -86,10 +121,79 @@ void NeuralNet::forward(const std::vector<float>& inputs)
     Layer& currentLayer = m_layers[l];
     Layer& previousLayer = m_layers[l-1];
     
-    //update weights for each neuron
-    for (size_t n = 0; n < currentLayer.size(); ++n)
-      currentLayer[n].updateWeights(previousLayer);
+    //update weights for each neuron, ignoring the bias neuron 
+    for (size_t n = 0; n < currentLayer.size()-1; ++n)
+      currentLayer[n].calculateOutput(previousLayer);
+  }
+}
+
+float NeuralNet::rmse(const std::vector<float>& expectedValues) const
+{
+  const Layer& outputLayer = m_layers.back();
+  float error = 0.f;
+
+  assert(expectedValues.size() == outputLayer.size()-1);
+  
+  for (size_t i = 0; i < expectedValues.size(); ++i)
+  {
+    float d  = expectedValues[i] - outputLayer[i].getOutput();
+    error += (d * d);
+  }
+  return std::sqrtf(error / float(expectedValues.size()));
+}
+
+void NeuralNet::back(const std::vector<float>& labels)
+{
+  //calculate rmse
+  float err = rmse(labels);
+
+  //calculate gradient at output layer
+  Layer& outputLayer = m_layers.back();
+  for (size_t i = 0; i < outputLayer.size(); ++i)
+    outputLayer[i].computeOutputGradients(labels[i]);
+
+  //calculate gradients of hidden layers
+  for (size_t i = m_layers.size() - 2; i > 0; --i)
+  {
+    Layer& hiddenLayer = m_layers[i];
+    Layer& nextLayer = m_layers[i+1];
+
+    for (size_t n = 0; n < hiddenLayer.size(); ++n)
+      hiddenLayer[n].computeHiddenGradients(nextLayer);
+
   }
 
+  //update weights of layers using gradients
+  for (size_t i = m_layers.size() - 1; i > 0; --i)
+  {
+    Layer& currentLayer = m_layers[i];
+    Layer& prevLayer= m_layers[i-1];
 
+    for(size_t n = 0; n < currentLayer.size()-1; ++n)
+      currentLayer[n].updateWeights(prevLayer);
+  }
+
+}
+
+std::vector<float> NeuralNet::output() const
+{
+  //copy values from output layer and return
+  const Layer& outputLayer = m_layers.back();
+  std::vector<float> result;
+  result.reserve(outputLayer.size()-1);
+
+  for (size_t i = 0; i < outputLayer.size() - 1; ++i)
+    result.push_back(outputLayer[i].getOutput());
+
+  return result;
+}
+
+std::vector<float> NeuralNet::predict(const std::vector<float>& inputs, const std::vector<float>& labels, unsigned numIterations)
+{
+  for (unsigned i = 0; i < numIterations; ++i)
+  {
+    forward(inputs);
+    back(labels);
+  }
+  return output();
 }
--- NeuralNetwork/NeuralNetwork/ann/neuralnet.h
+++ NeuralNetwork/NeuralNetwork/ann/neuralnet.h
@@ -8,8 +8,7 @@ class Neuron;
 struct Synapse
 {
   float weight;
-  unsigned from;
-  unsigned to;
+  float deltaWeight;
 };
 
 using ActivationFunc = float(*)(float);
@@ -19,7 +18,11 @@ class Neuron
 {
 public:
   Neuron(unsigned numOutputs, unsigned index);
-  void updateWeights(const Layer& previousLayer);
+  void calculateOutput(const Layer& previousLayer);
+  void computeOutputGradients(float expectedValue);
+  void computeHiddenGradients(const Layer& nextLayer);
+  void updateWeights(Layer& previousLayer);
+  
   void setOutput(float val);
   void setActivationFunctions(ActivationFunc func, ActivationFunc derivative);
   float getOutput() const;
@@ -28,6 +31,7 @@ private:
   ActivationFunc m_func = nullptr;
   ActivationFunc m_derivative = nullptr;
   float m_output;
+  float m_gradient;
   unsigned m_index;
   std::vector<Synapse> m_edges;
 };
@@ -39,12 +43,12 @@ class NeuralNet
 public:
   NeuralNet(const std::vector<unsigned>& config);
   Neuron& getNeuron(unsigned layerNum, unsigned index);
-  std::vector<float> predict(const std::vector<float>& inputs, const std::vector<float>& labels);
-
+  std::vector<float> predict(const std::vector<float>& inputs, const std::vector<float>& labels, unsigned numIter);
 
 private:
   void forward(const std::vector<float>& inputvalues);
   void back(const std::vector<float>& labels);
+  float rmse(const std::vector<float>& expectedValues) const;
   std::vector<float> output() const;
 
   std::vector<Layer> m_layers;
