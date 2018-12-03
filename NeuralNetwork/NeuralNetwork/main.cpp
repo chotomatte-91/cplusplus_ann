c5363eb8e725bb8b744e020541f9d614df09a91f
Chin Wei Boon <35475696+chinweiboon@users.noreply.github.com>
2018-12-03 23:11:28 +0800

    Add files via upload
    Edited main.cpp and laid the framework to extract data from CPU to allow GPU to take in for kernel calls

--- NeuralNetwork/NeuralNetwork/main.cpp
+++ NeuralNetwork/NeuralNetwork/main.cpp
@@ -5,80 +5,134 @@
 
 float mytanH(float val)
 {
-  return std::tanh(val);
+	return std::tanh(val);
 }
 
 float mytanHPrime(float val)
 {
-  return 1.f - (val * val);
+	return 1.f - (val * val);
 }
 
 void ann_cpu_test()
 {
-  //two neurons input, two neurons hidden, one neuron output
-  std::vector<unsigned> config{2, 2, 1};
-  NeuralNet nn(config);
+	//two neurons input, two neurons hidden, one neuron output
+	std::vector<unsigned> config{ 2, 2, 1 };
+	NeuralNet nn(config);
 
-  //hardcoded XOR input and labels
-  /*XOR TABLE
-  0 0 0
-  0 1 1
-  1 0 1
-  1 1 0
-  */
+	//hardcoded XOR input and labels
+	/*XOR TABLE
+	0 0 0
+	0 1 1
+	1 0 1
+	1 1 0
+	*/
 
-  std::vector<std::vector<float>> inputs = {
-    {0, 0},
-    {0, 1},
-    {1, 0},
-    {1, 1}
-  };
-  std::vector<float> labels = { 0, 1, 1, 0 };
+	std::vector<std::vector<float>> inputs = {
+		{ 0, 0 },
+	{ 0, 1 },
+	{ 1, 0 },
+	{ 1, 1 }
+	};
+	std::vector<float> labels = { 0, 1, 1, 0 };
 
-  //hardcoded initialization of weights for testing (last value is bias)
-  float i1_h1 = -0.7706f;
-  float i2_h1 = 0.6257f;
-  float h1_bias = 0.1859f;
-  float i1_h2 = 0.5607f;
-  float i2_h2 = 0.2109f;
-  float h2_bias = -0.7984f;
-  float h1_o1 = 0.5951f;
-  float h2_o1 = 0.3433f;
-  float o1_bias = 0.1328f;
+	//hardcoded initialization of weights for testing (last value is bias)
+	float i1_h1 = -0.7706f;
+	float i2_h1 = 0.6257f;
+	float h1_bias = 0.1859f;
+	float i1_h2 = 0.5607f;
+	float i2_h2 = 0.2109f;
+	float h2_bias = -0.7984f;
+	float h1_o1 = 0.5951f;
+	float h2_o1 = 0.3433f;
+	float o1_bias = 0.1328f;
 
 
-  std::vector<float> neuron_one_weights = { i1_h1, i2_h1, h1_bias };
-  std::vector<float> neuron_two_weights = { i1_h2, i2_h2, h2_bias };
-  std::vector<float> output_weights = { h1_o1, h2_o1, o1_bias }; 
+	std::vector<float> neuron_one_weights = { i1_h1, i2_h1, h1_bias };
+	std::vector<float> neuron_two_weights = { i1_h2, i2_h2, h2_bias };
+	std::vector<float> output_weights = { h1_o1, h2_o1, o1_bias };
 
-  /*
-  //set weights
-  for (size_t i = 0; i < 3; ++i)
-  {
-    //input to hidden layer
-    nn.getNeuron(0, 1).setWeight();
-    nn.getNeuron(0, 2).setWeight();
+	/*
+	//set weights
+	for (size_t i = 0; i < 3; ++i)
+	{
+	//input to hidden layer
+	nn.getNeuron(0, 1).setWeight();
+	nn.getNeuron(0, 2).setWeight();
 
-    //hidden to output layer
-    nn.getNeuron(1, 1).setWeight();
-    nn.getNeuron(1, 2).setWeight();
-  }
-  */
+	//hidden to output layer
+	nn.getNeuron(1, 1).setWeight();
+	nn.getNeuron(1, 2).setWeight();
+	}
+	*/
 
-  Neuron& neuron = nn.getNeuron(1, 1);
-  float y = neuron.getOutput();
-  neuron.setActivationFunctions(mytanH, mytanHPrime);
+	Neuron& neuron = nn.getNeuron(1, 1);
+	float y = neuron.getOutput();
+	neuron.setActivationFunctions(mytanH, mytanHPrime);
 }
-
 
 int main(int argc, char **argv)
 {
-  ann_cpu_test();
+	ann_cpu_test();
 #if 1
 	//if (argc != 2) {
 	//	printf("Usage: [image file]");
 	//	return -1;
 	//}
+
+	//Test test
+	std::vector<unsigned> test{ 2, 2, 1 };
+	NeuralNet testNetwork(test);
+
+	int numOfNeurons = 0;
+	std::vector<int> hiddenLayerIndex;
+	std::vector<float> weights;
+	//skip output layer
+	//for each layer
+	for (int i = 0; i < test.size() - 1; ++i)
+	{
+		//for each neuron
+		for (int j = 0; j < testNetwork.m_layers[i].size(); ++j)
+		{
+			Neuron temp = testNetwork.getNeuron(i, j);
+			//for each synapse
+			for (int k = 0; k < temp.m_edges.size(); ++k)
+			{
+				weights.push_back(temp.m_edges[k].weight);
+			}
+		}
+	}
+
+	std::vector<float> inputVal;
+	for (int i = 0; i < test[0]; ++i)
+	{
+		//get us float* vector; the input
+		inputVal.push_back(testNetwork.getNeuron(0, i).getOutput());
+	}
+
+	float * h_input = &inputVal[0];
+
+	//offset outselves to get correct set of weights
+	float * h_hiddenWeights = &weights[0];
+
+	//get the sizes of the inputMatrix and weightMatrix's height.
+	//weightMatrix size startVecLength * startMatHeight
+	//going forward, we can push in test/config ptr into GPU then we access the length value directly from GPU call
+	int startVecLength = test[0], startMatHeight = test[1];
+
+
+
+	////weightMatrix for 1st layer
+	//float * tempWeightMat = new float[numOfNeurons];
+	//
+	////update weightMatrix with synapse weight
+	//for (int i = 0; i < numOfNeurons; ++i)
+	//{
+	//	for (int j = 0; j < testNetwork.m_layers[i].size(); ++j)
+	//		tempWeightMat[i] = testNetwork.getNeuron(i, j).;
+	//}
+
+	//
+
 
 	StopWatchInterface *hTimer = NULL;
 	sdkCreateTimer(&hTimer);
@@ -109,9 +163,9 @@ int main(int argc, char **argv)
 	//uint byteCount = imageWidth * imageHeight * imageChannels * sizeof(unsigned char);
 
 	/*
-		----------------------------
-		VECTOR TO MATRIX DOT PRODUCT
-		----------------------------
+	----------------------------
+	VECTOR TO MATRIX DOT PRODUCT
+	----------------------------
 	*/
 	std::cout << "Starting GPU vector to matrix dot product: \n";
 	std::cout << "Length: " << std::atoi(argv[1]) << std::endl;
@@ -139,13 +193,13 @@ int main(int argc, char **argv)
 	checkCudaErrors(cudaMemcpy(d_arr1, h_arr1, vmSize * sizeof(float), cudaMemcpyHostToDevice));
 	checkCudaErrors(cudaMemcpy(d_arr2, h_arr2, vmSize * vmSize * sizeof(float), cudaMemcpyHostToDevice));
 
-	auto numberOfRows = (unsigned) ceilf(float(vmSize) / BLOCKSIZE);
+	auto numberOfRows = (unsigned)ceilf(float(vmSize) / BLOCKSIZE);
 	dim3 gridSize(numberOfRows, numberOfRows, 1);
 	dim3 blockSize(BLOCKSIZE, BLOCKSIZE, 1);
 	sdkResetTimer(&hTimer);
 	sdkStartTimer(&hTimer);
 
-	dotProduct <<< gridSize, blockSize >>>(d_arr1, d_arr2, d_output, vmSize);
+	dotProduct << < gridSize, blockSize >> >(d_arr1, d_arr2, d_output, vmSize);
 	checkCudaErrors(cudaDeviceSynchronize());
 
 	checkCudaErrors(cudaMemcpy(dh_output, d_output, vmSize * sizeof(float), cudaMemcpyDeviceToHost));
@@ -179,7 +233,6 @@ int main(int argc, char **argv)
 		std::cout << "Results match\n";
 		std::cout << "Speedup: " << CPUTime / GPUTime << std::endl;
 	}
-
 
 	// Clean up
 	checkCudaErrors(cudaFree(d_arr1));
